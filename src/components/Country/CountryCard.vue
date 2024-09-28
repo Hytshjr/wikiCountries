2328b359abbae5798011dbc8d07c89240bab4451
hytsh <celixscondolo@gmail.com>
2024-09-28 17:08:06 -0500

    feat: add countries list for scroll whitout lazy loading

---
+++ src/components/Country/CountryCard.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="countries__item">
+        <div class="countries__card">
+            <img v-if="country.image" :src="country.image" :alt="`Image of ${country.name}`" class="countries__image" />
+            <div class="countries__info">
+                <img :src="`https://flagcdn.com/h40/${country.code.toLowerCase()}.png`"
+                     :alt="`Flag of ${country.name}`" class="countries__flag" />
+                <div class="countries__details">
+                    <strong class="countries__name">{{ country.name }}</strong>
+                    <span class="countries__continent">{{ country.continent.name }}</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CountryCard',
+    props: {
+        country: {
+            type: Object,
+            required: true,
+        },
+    },
+};
+</script>
+
+<style scoped src="./CountryCard.css"></style>
---
+++ src/components/Country/CountryList.vue
@@ -0,0 +1,40 @@
+<template>
+    <main class="countries">
+        <h1 class="countries__title">Countries Information</h1>
+        <Row class="countries__list">
+            <CountryCard
+                v-for="country in countries"
+                :key="country.code"
+                :country="country"
+            />
+        </Row>
+        <div v-if="loading" class="countries__loading">Loading...</div>
+        <div v-if="error" class="countries__error">Error: {{ error.message }}</div>
+        <div v-if="countries.length === 0" class="countries__empty">No countries found.</div>
+    </main>
+</template>
+
+<script>
+import { useCountries } from './useCountries';
+import Row from '@/components/Grid/Row.vue';
+import CountryCard from './CountryCard.vue';
+
+export default {
+    name: 'CountryList',
+    components: {
+        Row,
+        CountryCard,
+    },
+    setup() {
+        const { countries, loading, error } = useCountries();
+
+        return {
+            countries,
+            loading,
+            error,
+        };
+    },
+};
+</script>
+
+<style scoped src="./CountryList.css"></style>
