7361568c946c824819b30c9037dc5e19e192ed40
hytsh <celixscondolo@gmail.com>
2024-09-28 17:18:18 -0500

    chore: add LazyLoad and update setting apollo client

--- src/main.js
+++ src/main.js
@@ -1,22 +1,17 @@
-import { createApp } from 'vue'
 import App from './App.vue'
-import {  provideApolloClient } from '@vue/apollo-composable';
-import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client/core';
 import router from './router';
+import { createApp } from 'vue'
 import '@/assets/style/style.css';
-
-const httpLink = createHttpLink({
-    uri: 'https://countries.trevorblades.com/',
-  });
-
-  const apolloClient = new ApolloClient({
-    link: httpLink,
-    cache: new InMemoryCache(),
-  });
+import VueLazyload from 'vue-lazyload';
+import apolloClient from '@/graphql/apollo-client'
+import {  provideApolloClient } from '@vue/apollo-composable';
 
 const app = createApp(App);
 
 provideApolloClient(apolloClient);
-
+app.use(VueLazyload, {
+  loading: '/path-to-loading-image',
+  error: '/path-to-error-image',
+});
 app.use(router);
 app.mount('#app');
