4651d70b287aa143b3e7c2854d0e3b72f976e677
hytsh <celixscondolo@gmail.com>
2024-09-28 13:23:47 -0500

    feat(grid): add grid components

--- src/components/Grid/Column.vue
+++ src/components/Grid/Column.vue
@@ -6,7 +6,7 @@
 
 <script>
 export default {
-    name: 'Column',
+    name: 'ColumnGrid',
     props: {
         size: {
             type: Number,
--- src/components/Grid/Grid.vue
+++ src/components/Grid/Grid.vue
@@ -8,6 +8,6 @@
 
 <script>
 export default {
-    name: 'Grid',
+    name: 'GridMain',
 }
 </script>
