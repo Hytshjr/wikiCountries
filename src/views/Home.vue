072a64521aba5025b386353cae51bcbdb9ebd18a
hytsh <celixscondolo@gmail.com>
2024-09-28 17:10:04 -0500

    feat: integrate countries list on home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,26 @@
 <template>
-    <slot></slot>
+    <Container>
+        <Row>
+            <Column :cols="12">
+                <CountryList/>
+            </Column>
+        </Row>
+    </Container>
 </template>
 
 <script>
+import Container from '@/components/Grid/Container.vue';
+import Row from '@/components/Grid/Row.vue';
+import Column from '@/components/Grid/Column.vue';
+import CountryList from '@/components/Country/CountryList.vue';
+
 export default {
     name: 'HomeWiki',
-}
+    components: {
+        Container,
+        Row,
+        Column,
+        CountryList,
+    },
+};
 </script>
